0654eb576dcf224f5abb937e4d79725b74baefab
Phil Nash <github@philnash.me>
2025-07-23 22:39:08 +0100

    Slightly tweaked parse_enum_from_function

--- catchkit/src/catchkit_stringify.cpp
+++ catchkit/src/catchkit_stringify.cpp
@@ -10,15 +10,14 @@
 namespace CatchKit::Detail {
 
     auto parse_enum_name_from_function(std::string_view function_name, bool fully_qualified) -> std::string_view {
-        auto start = function_name.find("candidate = ");
-        if (start != std::string_view::npos) {
+        if( auto start = function_name.find("candidate = "); start != std::string_view::npos ) {
             start += 12;
-            auto end = function_name.find_first_of(",;", start);
-            if (end != std::string_view::npos) {
+            if( auto end = function_name.find_first_of(",;", start); end != std::string_view::npos ) {
                 auto qualified_enum_name = function_name.substr(start, end-start);
                 if( fully_qualified )
                     return qualified_enum_name;
                 auto last_colon = qualified_enum_name.find_last_of(':');
+                assert(last_colon !=  std::string_view::npos);
                 return qualified_enum_name.substr(last_colon+1);
             }
         }
