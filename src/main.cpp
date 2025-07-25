e033fab794f2c6c5be446d8ed838a5cd08536c4d
Phil Nash <github@philnash.me>
2025-07-25 14:54:32 +0100

    Added ability to run a single test by name

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,20 @@
 
 int main() {
     CatchKit::ConsoleReporter reporter(CatchKit::ReportOn::FailuresOnly);
-    run_tests(CatchKit::Detail::get_all_tests(), reporter);
+
+    std::string only_run_test; // = "section within a generator";
+
+    if( only_run_test.empty()) {
+        run_tests(CatchKit::Detail::get_all_tests(), reporter);
+    }
+    else {
+        std::vector<CatchKit::Detail::Test> const& tests = CatchKit::Detail::get_all_tests();
+        std::vector<CatchKit::Detail::Test> tests_to_run;
+        for (auto const& test : tests) {
+            if( test.test_info.name == only_run_test )
+                tests_to_run.push_back(test);
+        }
+        assert(!tests_to_run.empty());
+        run_tests(tests_to_run, reporter);
+    }
 }
