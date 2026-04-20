6eb064a7c5c6d93a4a2f0a62eeded42f0eb98d4c
Phil Nash <github@philnash.me>
2026-04-20 07:51:59 +0100

    Fix formatter for ExpressionInfo in release builds

--- catchkit/src/expression_info.cpp
+++ catchkit/src/expression_info.cpp
@@ -5,6 +5,7 @@
 #include "catchkit/expression_info.h"
 
 #include <cassert>
+#include <utility>
 
 std::format_context::iterator std::formatter<CatchKit::ExpressionInfo>::format(CatchKit::ExpressionInfo const& expr, std::format_context& ctx) {
     using namespace CatchKit;
@@ -13,12 +14,19 @@ std::format_context::iterator std::formatter<CatchKit::ExpressionInfo>::format(C
         [&]<typename T>(T const& expr_inner) -> std::format_context::iterator {
             if constexpr(std::is_same_v<T, UnaryExpressionInfo>)
                 return std::format_to( ctx.out(), "{}", expr_inner.value );
-            if constexpr(std::is_same_v<T, BinaryExpressionInfo>)
+            else if constexpr(std::is_same_v<T, BinaryExpressionInfo>)
                 return std::format_to( ctx.out(), "{} {} {}", expr_inner.lhs, expr_inner.op, expr_inner.rhs );
-            if constexpr(std::is_same_v<T, MatchExpressionInfo>)
+            else if constexpr(std::is_same_v<T, MatchExpressionInfo>)
                 return std::format_to( ctx.out(), "{} {}", expr_inner.candidate_value, expr_inner.matcher );
-            if constexpr(std::is_same_v<T, ExceptionExpressionInfo>)
+            else if constexpr(std::is_same_v<T, ExceptionExpressionInfo>)
                 return std::format_to( ctx.out(), "{}", expr_inner.exception_message );
-            assert(false);
+            else if constexpr(std::is_same_v<T, ExpectationExpressionInfo>) {
+                assert(false); // Not yet implemented
+                return std::format_to( ctx.out(), "{}", "{not implemented}" );
+            }
+            else {
+                static_assert(std::is_same_v<T, monostate>);
+                std::unreachable();
+            }
     }, expr);
 }
